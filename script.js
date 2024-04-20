cac9d052228c615fd69e7d1a84fd3c4586f0660f
Kalpanabharath <stonepanakeerthu@gmail.com>
2024-04-20 16:15:49 +0530

    display type one

--- script.js
+++ script.js
@@ -1,11 +1,50 @@
+document.addEventListener("DOMContentLoaded", function () {
+    showProducts('Men');
+   let menEl= document.getElementById('Mencategory');
+  menEl.classList.add("clicked")
+});
+
+function showProducts(category) {
+    fetch('https://cdn.shopify.com/s/files/1/0564/3685/0790/files/multiProduct.json')
+        .then(response => response.json())
+        .then(data => {
+            const categoryData = data.categories.find(cat => cat.category_name === category);
+
+            if (categoryData) {
+                document.getElementById('cardcontainer').innerHTML = '';
+
+                categoryData.category_products.forEach(product => {
+                    createProductCard(product);
+                });
+            }
+        })
+        .catch(error => console.error('Error fetching data:', error));
+    }
+    // showProducts('Men');
+
+// card container
+function createProductCard(product){
+
 let cardEl=document.createElement('div');
 cardEl.className='card';
 
 let productImage = document.createElement('img');
-productImage.src = 'https://assets.myntassets.com/h_720,q_90,w_540/v1/assets/images/22462330/2023/3/22/27a1d087-dc81-46a0-9b86-505f6491b5931679431326265RoyalChallengersBangalore2023MensReplicaJersey1.jpg';
-productImage.alt = "image";
+productImage.src = product.image;
+productImage.alt = "product image";
 productImage.className = 'productimage';
 cardEl.appendChild(productImage);
+
+
+
+
+if (product.badge_text !== null) {
+    const badge = document.createElement('div');
+    badge.className = 'badge';
+    badge.innerText = product.badge_text;
+    cardEl.appendChild(badge);
+}
+
+
 
 
 // content lineone
@@ -14,11 +53,11 @@ let contentLineone=document.createElement('div');
 contentLineone.className='contentLineone';
 
 let productName=document.createElement('h3');
-productName.innerText="curta"
+productName.innerText=`${product.title}`;
 contentLineone.appendChild(productName);
 
 let vendorName=document.createElement('p');
-vendorName.textContent=`• buma`;
+vendorName.textContent=`• ${product.vendor}`;
 contentLineone.appendChild(vendorName);
 
 cardEl.appendChild(contentLineone);
@@ -28,16 +67,17 @@ let contentLinetwo=document.createElement('div');
 contentLinetwo.className='contentLineone';
 
 let price=document.createElement('p');
-price.textContent=` Rs 1119.00`;
+price.textContent=` Rs ${product.price}.00`;
 contentLinetwo.appendChild(price);
 
 let compareprice=document.createElement('p');
-compareprice.textContent=` Rs 1119.00`;
+compareprice.textContent=`${product.compare_at_price}.00`;
 contentLinetwo.appendChild(compareprice);
 compareprice.classList.add('linethrough');
 
 let discount=document.createElement('p');
-discount.textContent=` 50% off`;
+let discountPercentage=calculateDiscountPercentage(product.price, product.compare_at_price);
+discount.textContent=` ${discountPercentage}% off`
 contentLinetwo.appendChild(discount);
 discount.classList.add('discount');
 
@@ -52,3 +92,11 @@ cardEl.appendChild(btnEl)
 
 
 document.getElementById('cardcontainer').appendChild(cardEl);
+
+}
+
+function calculateDiscountPercentage(price, compareAtPrice) {
+    const discount = ((compareAtPrice - price) / compareAtPrice) * 100;
+    return Math.round(discount);
+}
+
